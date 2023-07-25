d7b4265d9d09b13c9ccda66a4209fa6a4875d6e5
safo samson <safosamson18@gmail.com>
2023-07-25 11:47:52 +0100

    lines underneath each button

--- src/components/WaitingCustomersUI/VerticalContainer.jsx
+++ src/components/WaitingCustomersUI/VerticalContainer.jsx
@@ -3,10 +3,28 @@ import './VerticalContainer.css';
 const VerticalContainer = () => {
   return (
     <div className="vertical-container">
-      <button className="button">Waiting      </button>
-      <button className="button">Accepted      </button>
+
+      <div class="button-container">
+          <button className="button">Waiting </button>
+         <div class="line"></div>
+      </div> 
+
+      <div class="button-container">
+          <button className="button">Accepted </button>
+         <div class="line"></div>
+      </div> 
+
+      <div class="button-container">  
       <button className="button">Rejected</button>
+         <div class="line"></div>
+      </div> 
+
+      <div class="button-container">
       <button className="button">Help</button>
+      </div> 
+      
+     
+
     </div>
   );
 };
