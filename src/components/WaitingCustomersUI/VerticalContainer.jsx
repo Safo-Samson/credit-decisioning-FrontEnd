9a357b489030529e632d84c61fed00306b02e516
Safo Samson <safosamson18@gmail.com>
2023-07-26 14:02:14 +0100

    fixing the side buttons

--- src/components/WaitingCustomersUI/VerticalContainer.jsx
+++ src/components/WaitingCustomersUI/VerticalContainer.jsx
@@ -1,38 +1,32 @@
-import './VerticalContainer.css';
+import "./VerticalContainer.css";
 
 const VerticalContainer = () => {
-  return (
-    <div className="vertical-container">
+	return (
+		<div className="vertical-container">
+			<div className="button-container">
+				<button className="v-button">Waiting </button>
+				<div className="line"></div>
+			</div>
 
-      <div className="button-container">
-          <button className="button">Waiting </button>
-         <div className="line"></div>
-      </div> 
+			<div className="button-container">
+				<button className="v-button">Accepted </button>
+				<div className="line"></div>
+			</div>
 
-      <div className="button-container">
-          <button className="button">Accepted </button>
-         <div className="line"></div>
-      </div> 
+			<div className="button-container">
+				<button className="v-button">Rejected</button>
+				<div className="line"></div>
+			</div>
 
-      <div className="button-container">
-         
-      <button className="button">Rejected</button>
-         <div className="line"></div>
-      </div> 
+			<div className="button-container">
+				<button className="v-button">Help</button>
+				<div className="line"></div>
+			</div>
 
-      <div className="button-container">
-      <button className="button">Help</button>
-      </div> 
-      
-     
-    
-        <button className="logout-button">Logout</button>
-        <div className="line"></div>
-      
-     
-
-    </div>
-  );
+			<button className="logout-button">Logout</button>
+			<div className="line"></div>
+		</div>
+	);
 };
 
 export default VerticalContainer;
