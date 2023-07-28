e3e546534a5a1a879bdb0ff139bd14e91618e09d
safo samson <safosamson18@gmail.com>
2023-07-28 12:06:41 +0100

    minor

--- src/components/Credit Score/CreditScoreResult.jsx
+++ src/components/Credit Score/CreditScoreResult.jsx
@@ -5,55 +5,61 @@ import { faIdBadge } from "@fortawesome/free-solid-svg-icons";
 import CreditScoreSemiCircle from "./ProgressSemiCircle";
 import ProgressCircle from "./ProgressCircle";
 import ProgressSemiCircle from "./ProgressSemiCircle";
+import ScoreSummary from "./ScoreSummary";
 
 const CreditScoreResult = (props) => {
   return (
-    <div className="credit-container">
-      <div className="credit-remaining-content">
-        <div className="deets-icon">
-          <FontAwesomeIcon icon={faIdBadge} />
+    <>
+      <div className="credit-container">
+        <div className="credit-remaining-content">
+          <div className="deets-icon">
+            <FontAwesomeIcon icon={faIdBadge} />
+          </div>
+
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <div className="text-container in-text">
+              <b>First Name</b>
+              <br></br>
+              {props.customers[0].name}
+              <br></br>
+              <div className="text-line "></div>
+            </div>
+
+            <div className="text-container in-text">
+              <b>Last Name</b>
+              <br></br>
+              {props.customers[0].name}
+              <br></br>
+              <div className="text-line "></div>
+            </div>
+
+            <div className="text-container in-text">
+              <b>Account No.</b>
+              <br></br>
+              {props.customers[0].AccountNo}
+              <br></br>
+              <div className="text-line "></div>
+            </div>
+
+            <div className="text-container in-text">
+              <b>Date Applied</b>
+              <br></br>
+              {props.customers[0].dateApplied.toLocaleString()}
+              <br></br>
+              <div className="text-line "></div>
+            </div>
+
+            <button className="back-button">Back</button>
+          </div>
         </div>
 
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <div className="text-container in-text">
-            <b>First Name</b>
-            <br></br>
-            {props.customers[0].name}
-            <br></br>
-            <div className="text-line "></div>
-          </div>
-
-          <div className="text-container in-text">
-            <b>Last Name</b>
-            <br></br>
-            {props.customers[0].name}
-            <br></br>
-            <div className="text-line "></div>
-          </div>
-
-          <div className="text-container in-text">
-            <b>Account No.</b>
-            <br></br>
-            {props.customers[0].AccountNo}
-            <br></br>
-            <div className="text-line "></div>
-          </div>
-
-          <div className="text-container in-text">
-            <b>Date Applied</b>
-            <br></br>
-            {props.customers[0].dateApplied.toLocaleString()}
-            <br></br>
-            <div className="text-line "></div>
-          </div>
-
-          <button className="back-button">Back</button>
+        <div className="progress-containers">
+          <ProgressSemiCircle progress={84} />
+          <ProgressCircle progress={92} />
+          <ScoreSummary />
         </div>
       </div>
-
-      <ProgressSemiCircle progress={84} />
-      <ProgressCircle progress={92} />
-    </div>
+    </>
   );
 };
 
---
+++ src/components/Credit Score/ScoreSummary.jsx
@@ -0,0 +1,11 @@
+import "./ScoreSummary.css";
+
+const ScoreSummary = (props) => {
+  return (
+    <div className="summary-display-container">
+      <button className="summary-button">Summary</button>
+    </div>
+  );
+};
+
+export default ScoreSummary;
