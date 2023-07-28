c4d0ac7c78417de434bac594bdde0a9a47ec736d
safo samson <safosamson18@gmail.com>
2023-07-28 15:58:05 +0100

    making tthe grey container on the waiting customer

--- src/components/WaitingCustomersUI/ApplicationUI.jsx
+++ src/components/WaitingCustomersUI/ApplicationUI.jsx
@@ -5,27 +5,23 @@ import VerticalContainer from "../WaitingCustomersUI/VerticalContainer";
 const AppUI = (props) => {
 	
 	return (
-		<div className='app-container'>
-			
-			<VerticalContainer customers={props.customers} />
-			
-			<div className='remaining-content'>
+    <div className="app-container">
+      <VerticalContainer customers={props.customers} />
 
-				{props.customers.map((customer) =>
-					<CusApplication
-					key={customer.id} // very important to add key
-					AccountNo={customer.AccountNo}
-					dateApplied={customer.dateApplied}
-					amount={customer.amount}
-					loanType={customer.loanType}
-					name={customer.name}
-					
-					/>)};
-				
-			</div>
-			
-		</div>
-	);
+      <div className="remaining-content">
+        {props.customers.map((customer) => (
+          <CusApplication
+            key={customer.id} // very important to add key
+            AccountNo={customer.AccountNo}
+            dateApplied={customer.dateApplied}
+            amount={customer.amount}
+            loanType={customer.loanType}
+            name={customer.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AppUI;
