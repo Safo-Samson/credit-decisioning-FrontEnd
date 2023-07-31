fb18d65e9cecec777c7b610b00bf90419e2de1f0
Safo Samson <safosamson18@gmail.com>
2023-07-31 12:24:07 +0100

    nibnr

--- src/components/Credit Score/CreditScoreResult.jsx
+++ src/components/Credit Score/CreditScoreResult.jsx
@@ -5,21 +5,23 @@ import ProgressSemiCircle from "./ProgressSemiCircle";
 import ScoreSummary from "./ScoreSummary";
 
 const CreditScoreResult = (props) => {
-  return (
-    <>
-      <div className="credit-container">
-        <div className="credit-remaining-content">
-          <div className="progress-containers">
-            <ProgressSemiCircle progress={84} />
-            <ProgressCircle progress={92} />
-            <ScoreSummary />
-          </div>
-          <button className="confirm-accept">accept</button>
-          <button className="confirm-reject">accept</button>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="credit-container">
+				<div className="credit-remaining-content">
+					<div className="progress-containers">
+						<div className="creditScore">
+							<ProgressSemiCircle progress={84} />
+						</div>
+						<ProgressCircle progress={92} />
+						<ScoreSummary />
+					</div>
+					<button className="confirm-accept">accept</button>
+					<button className="confirm-reject">reject</button>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default CreditScoreResult;
