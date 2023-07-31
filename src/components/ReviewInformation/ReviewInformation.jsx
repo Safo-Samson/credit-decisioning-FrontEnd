261df2026d9ec8da1a07223897138a95ad746106
emiunal <E.unal2@newcastle.ac.uk>
2023-07-31 12:06:30 +0100

    fix review information styles

--- src/components/ReviewInformation/ReviewInformation.jsx
+++ src/components/ReviewInformation/ReviewInformation.jsx
@@ -14,6 +14,8 @@ function ReviewInformation(props){
     const handleClick = (e) => {
         e.preventDefault();
         navigate("/CreditScore");
+        console.log(firstName)
+        
 
     }
 
@@ -34,28 +36,30 @@ function ReviewInformation(props){
             <form className="form-container">
                 
                 <div className="firstRow">
-                <div className="firstName">
-                <label htmlFor="firstName"> First Name</label>
-                <input value={firstName} onChange={(e)=>setFirst(e.target.value)}type="firstName"placeholder="" id= "firstName" name="firstName"/>
+                    <div className="firstName">
+                    <label htmlFor="firstName"> First Name</label>
+                    <input value={props.customers[0].name} onChange={(e)=>setFirst(e.target.value)}type="firstName"placeholder="" id= "firstName" name="firstName"/>
+                    </div>
+                    <div className="lastName">
+                    <label htmlFor="lastName"> Last Name</label>
+                    <input value={lastName} onChange={(e)=>setLast(e.target.value)}type="lastName"placeholder="" id= "lastName" name="lastName"/>
+                    </div>
+                    <div className="loanType">
+                    <label htmlFor="loanType"> Loan Type</label>
+                    <input value={loanType} onChange={(e)=>setType(e.target.value)}type="loanType"placeholder="" id= "loanType" name="loanType"/>
+                    </div>
+                    <div className="loanAmount">
+                    <label htmlFor="loanAmount"> Loan Amount</label>
+                    <input value={loanAmount} onChange={(e)=>setAmount(e.target.value)}type="loanAmount"placeholder="" id= "loanAmount" name="loanAmount"/>
+                    </div>
                 </div>
-                <div className="lastName">
-                <label htmlFor="lastName"> Last Name</label>
-                <input value={lastName} onChange={(e)=>setLast(e.target.value)}type="lastName"placeholder="" id= "lastName" name="lastName"/>
-                </div>
-                <div className="loanType">
-                <label htmlFor="loanType"> Loan Type</label>
-                <input value={loanType} onChange={(e)=>setType(e.target.value)}type="loanType"placeholder="" id= "loanType" name="loanType"/>
-                </div>
-                <div className="loanAmount">
-                <label htmlFor="loanAmount"> Loan Amount</label>
-                <input value={loanAmount} onChange={(e)=>setAmount(e.target.value)}type="loanAmount"placeholder="" id= "loanAmount" name="loanAmount"/>
-                </div>
-                </div>
+
+
                 < div className="secondRow">
-                <div className="date">
-                <label htmlFor="date"> Date Applied </label>
-                <input value={date} onChange={(e)=>setValue(e.target.value)}type="date"placeholder="" id= "date" name="date"/>
-                </div>
+                    <div className="dateApplied">
+                    <label htmlFor="date"> Date Applied </label>
+                    <input value={date} onChange={(e)=>setValue(e.target.value)}type="date"placeholder="" id= "date" name="date"/>
+                    </div>
                 </div>
                 <button className="closeBTN">Close</button>
                 <button className="calculateBTN" onClick={handleClick}>Calculate Score</button>
