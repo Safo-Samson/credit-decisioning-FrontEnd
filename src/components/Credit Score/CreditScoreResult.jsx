7ba3d69418f19507badb886b781061af43e416a2
Safo Samson <safosamson18@gmail.com>
2023-07-31 09:10:58 +0100

    changing accept to reject button

--- src/components/Credit Score/CreditScoreResult.jsx
+++ src/components/Credit Score/CreditScoreResult.jsx
@@ -5,21 +5,21 @@ import ProgressSemiCircle from "./ProgressSemiCircle";
 import ScoreSummary from "./ScoreSummary";
 
 const CreditScoreResult = (props) => {
-  return (
-    <>
-      <div className="credit-container">
-        <div className="credit-remaining-content">
-          <div className="progress-containers">
-            <ProgressSemiCircle progress={84} />
-            <ProgressCircle progress={92} />
-            <ScoreSummary />
-          </div>
-          <button className="confirm-accept">accept</button>
-          <button className="confirm-reject">accept</button>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="credit-container">
+				<div className="credit-remaining-content">
+					<div className="progress-containers">
+						<ProgressSemiCircle progress={84} />
+						<ProgressCircle progress={92} />
+						<ScoreSummary />
+					</div>
+					<button className="confirm-accept">accept</button>
+					<button className="confirm-reject">reject</button>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default CreditScoreResult;
