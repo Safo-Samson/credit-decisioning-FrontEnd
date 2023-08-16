c721ddc0c52156a80a9e0d4f4dd0fb892a4c9404
safosamson <safosamson18@gmail.com>
2023-08-16 11:46:21 +0100

    did link to logout and dynamic summary

--- src/components/WaitingCustomersUI/HorizontalContainer.jsx
+++ src/components/WaitingCustomersUI/HorizontalContainer.jsx
@@ -2,28 +2,30 @@ import "./HorizontalContainer.css";
 import horseLogo from "../../assets/horseLogo.jpeg";
 import SearchBox from "./SearchBox";
 import React from "react";
-
+import { Link } from "react-router-dom";
 const HorizontalContainer = () => {
-	return (
-		<div className="horizontal-container">
-			<div className="logo-container">
-				<img src={horseLogo} alt="Lloyds horse logo" className="bank-logo" />
-				<div className="logo-text">Lloyds bank</div>
-			</div>
-			<SearchBox />
-			{/* <i className="fa fa-user profile-icon"></i> */}
+  return (
+    <div className="horizontal-container">
+      <div className="logo-container">
+        <img src={horseLogo} alt="Lloyds horse logo" className="bank-logo" />
+        <div className="logo-text">Lloyds bank</div>
+      </div>
+      <SearchBox />
+      {/* <i className="fa fa-user profile-icon"></i> */}
 
-			<div className="custom-dropdown">
-				<div className="dropdown-header"><i className="fa fa-user profile-icon"></i></div>
-					<div className="dropdown-content">
-						<a href="#">My Account</a>
-						<a href="#">Privacy & Security</a>
-						<a href="#">Help</a>
-						<a href="#">Logout</a>
-					</div>
-			</div>
-		</div>
-	);
+      <div className="custom-dropdown">
+        <div className="dropdown-header">
+          <i className="fa fa-user profile-icon"></i>
+        </div>
+        <div className="dropdown-content">
+          <a href="#">My Account</a>
+          <a href="#">Privacy & Security</a>
+          <a href="#">Help</a>
+          <Link to="/">Logout</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default React.memo(HorizontalContainer);
